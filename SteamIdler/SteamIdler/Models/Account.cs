3286ec82fe356b4ac64e41e92c3da9738dce72e9
Choi-UngGyu <cdr982116@gmail.com>
2020-12-14 10:05:25 +0900

    SteamIdler 프로젝트 제거
    steam 라이브러리는 윈도우의 레지스트리를 참조하므로 크로스 플랫폼 불가

--- SteamIdler/SteamIdler/Models/Account.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SteamIdler.Models
-{
-    public class Account
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string LoginKey { get; set; }
-    }
-}
